5977a45769ea5fdd90ddfb5ad43409f8acb39124
v-m-dev <morozvitalii2403@gmail.com>
2024-01-03 16:01:04 +0200

    feat: complete sortByAsc function

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -414,8 +414,18 @@ function rotateMatrix(matrix) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  const array = arr;
+  for (let i = 0; i < array.length; i += 1) {
+    for (let j = i; j < array.length; j += 1) {
+      if (array[i] > array[j]) {
+        const temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    }
+  }
+  return array;
 }
 
 /**
